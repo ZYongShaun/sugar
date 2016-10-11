ea18f7ab9a81fc420eff3ec124c18962817139d0
shaun.zyong <shaun.zyong@gmail.com>
2016-10-11 18:35:29 +0800

    modi

---
+++ jslib/uploadImg/demo/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': '3HcA_wHkRblRUM32nCC0gWZ22PF3_fAXjCgUdpNT',
+    'SECRET_KEY': 'P34dfP2GJ0JBV0kTNc5WPseDhC71M3CxR0XjE590',
+    'Bucket_Name': 'evernote',
+    'Port': 19110,
+    'Uptoken_Url': 'uptoken',
+    'Domain': 'http://o84gzkc9y.bkt.clouddn.com/'
+};
