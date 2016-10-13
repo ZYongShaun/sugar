11eb389438dab2bb4f0c23d9103876916e269ccd
shaun.zyong <shaun.zyong@gmail.com>
2016-10-13 14:28:22 +0800

    add token

--- js/addArticle.js
+++ js/addArticle.js
@@ -4,12 +4,13 @@
 var addArticle = {};
 
 addArticle.init = function () {
+    addArticle.token = Token().getToken();
     addArticle.render();
     addArticle.uploadImage();
 
     $('#message').off().on("click", function () {
         var $gitInfo = $('.gitInfo');
-        if($gitInfo.is(':hidden')){
+        if ($gitInfo.is(':hidden')) {
             $gitInfo.show();
         } else {
             $gitInfo.hide();
@@ -28,22 +29,21 @@ addArticle.uploadImage = function () {
         // 在初始化时，uptoken，uptoken_url，uptoken_func三个参数中必须有一个被设置
         // 切如果提供了多个，其优先级为uptoken > uptoken_url > uptoken_func
         // 其中uptoken是直接提供上传凭证，uptoken_url是提供了获取上传凭证的地址，如果需要定制获取uptoken的过程则可以设置uptoken_func
-        uptoken: '3HcA_wHkRblRUM32nCC0gWZ22PF3_fAXjCgUdpNT:s8sTOcFRZXgeeuOoiyPcZ-VWjU8=:eyJzY29wZSI6ImV2ZXJub3RlIiwiZGVhZGxpbmUiOjE0NzYxODc3ODd9', // uptoken是上传凭证，由其他程序生成
+        uptoken: addArticle.token, // uptoken是上传凭证，由其他程序生成
         // uptoken_url: '/uptoken',         // Ajax请求uptoken的Url，强烈建议设置（服务端提供）
         // uptoken_func: function(file){    // 在需要获取uptoken时，该方法会被调用
         //    // do something
         //    return uptoken;
         // },
         get_new_uptoken: false,             // 设置上传文件的时候是否每次都重新获取新的uptoken
-        // downtoken_url: '/downtoken',
-        // Ajax请求downToken的Url，私有空间时使用，JS-SDK将向该地址POST文件的key和domain，服务端返回的JSON必须包含url字段，url值为该文件的下载地址
-//             unique_names: true,              // 默认false，key为文件名。若开启该选项，JS-SDK会为每个文件自动生成key（文件名）
+        // downtoken_url: '/downtoken', // Ajax请求downToken的Url，私有空间时使用，JS-SDK将向该地址POST文件的key和domain，服务端返回的JSON必须包含url字段，url值为该文件的下载地址
+        // unique_names: true,              // 默认false，key为文件名。若开启该选项，JS-SDK会为每个文件自动生成key（文件名）
         // save_key: true,                  // 默认false。若在服务端生成uptoken的上传策略中指定了sava_key，则开启，SDK在前端将不对key进行任何处理
         domain: 'http://o84gzkc9y.bkt.clouddn.com/',     // bucket域名，下载资源时用到，必需
         container: 'container',             // 上传区域DOM ID，默认是browser_button的父元素
         max_file_size: '10mb',             // 最大文件体积限制
         flash_swf_url: 'path/of/plupload/Moxie.swf',  //引入flash，相对路径
-        max_retries: 3,                     // 上传失败最大重试次数
+        max_retries: 1,                     // 上传失败最大重试次数
         dragdrop: true,                     // 开启可拖曳上传
         drop_element: 'container',          // 拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
         chunk_size: '4mb',                  // 分块上传时，每块的体积
@@ -89,7 +89,7 @@ addArticle.uploadImage = function () {
                 $('#fileList').append(temp)
             },
             'Error': function (up, err, errTip) {
-                //上传出错时，处理相关的事情
+                addArticle.token = Token().getToken();
             },
             'UploadComplete': function () {
                 //队列文件处理完毕后，处理相关的事情
@@ -97,8 +97,9 @@ addArticle.uploadImage = function () {
             'Key': function (up, file) {
                 // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
                 // 该配置必须要在unique_names: false，save_key: false时才生效
-
-                var key = new Date().getTime();
+                var fileType = file.name.substr(file.name.lastIndexOf('.'));
+                var now = new Date();
+                var key = '0' + now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '/' + now.getTime() + fileType;
                 // do something with key here
                 return key
             }
@@ -108,7 +109,7 @@ addArticle.uploadImage = function () {
 // domain为七牛空间对应的域名，选择某个空间后，可通过 空间设置->基本设置->域名设置 查看获取
 
 // uploader为一个plupload对象，继承了所有plupload的方法
-}
+};
 
 addArticle.formatTime = function () {
     var now = new Date();
